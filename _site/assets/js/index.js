2878ade7558114d87ad7fbd211eeef6e8311f53e
Viet Anh Tong <vietanhtong1508@gmail.com>
2020-07-20 16:09:25 +0700

    add Back to top button

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -1,25 +1,46 @@
-const dropdown = $('.dropdown');
-const dropdownToggle = $('.dropdown-toggle');
-const dropdownMenu = $('.dropdown-menu');
-const showClass = 'show';
+$(() => {
+  const dropdown = $('.dropdown');
+  const dropdownToggle = $('.dropdown-toggle');
+  const dropdownMenu = $('.dropdown-menu');
+  const showClass = 'show';
 
-$(window).on('load resize', function () {
-  if (this.matchMedia('(min-width: 768px)').matches) {
-    dropdown.hover(
-      function () {
-        const self = $(this);
-        self.addClass(showClass);
-        self.find(dropdownToggle).attr('aria-expanded', 'true');
-        self.find(dropdownMenu).addClass(showClass);
+  $(window).on('load resize', function () {
+    if (this.matchMedia('(min-width: 768px)').matches) {
+      dropdown.hover(
+        function () {
+          const self = $(this);
+          self.addClass(showClass);
+          self.find(dropdownToggle).attr('aria-expanded', 'true');
+          self.find(dropdownMenu).addClass(showClass);
+        },
+        function () {
+          const self = $(this);
+          self.removeClass(showClass);
+          self.find(dropdownToggle).attr('aria-expanded', 'false');
+          self.find(dropdownMenu).removeClass(showClass);
+        }
+      );
+    } else {
+      dropdown.off('mouseenter mouseleave');
+    }
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+    } else {
+      $('#back-to-top').fadeOut();
+    }
+  });
+
+  // scroll body to 0px on click
+  $('#back-to-top').click(function () {
+    $('body,html').animate(
+      {
+        scrollTop: 0,
       },
-      function () {
-        const self = $(this);
-        self.removeClass(showClass);
-        self.find(dropdownToggle).attr('aria-expanded', 'false');
-        self.find(dropdownMenu).removeClass(showClass);
-      }
+      400
     );
-  } else {
-    dropdown.off('mouseenter mouseleave');
-  }
+    return false;
+  });
 });
