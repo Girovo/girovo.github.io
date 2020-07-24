f51dbb8873ab251cfd7ee88661bf96a734e3054e
Viet Anh Tong <vietanhtong1508@gmail.com>
2020-07-24 20:58:14 +0700

    handle smooth scroll and auto hide navbar on scroll down

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,6 +33,83 @@ $(() => {
     }
   });
 
+  // Smooth on external page
+  $(function () {
+    setTimeout(function () {
+      if (location.hash) {
+        /* we need to scroll to the top of the window first, because the browser will always jump to the anchor first before JavaScript is ready, thanks Stack Overflow: http://stackoverflow.com/a/3659116 */
+        window.scrollTo(0, 0);
+        target = location.hash.split('#');
+        smoothScrollTo($('#' + target[1]));
+      }
+    }, 1);
+
+    // taken from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    $('a[href*=\\#]:not([href=\\#])').click(function () {
+      if (
+        location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        smoothScrollTo($(this.hash));
+        return false;
+      }
+    });
+
+    function smoothScrollTo(target) {
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+      if (target.length) {
+        $('html,body').animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000
+        );
+      }
+    }
+  });
+
+  // Hide Header on on scroll down
+  let didScroll;
+  let lastScrollTop = 0;
+  let delta = 5;
+  let navbarHeight = $('nav').outerHeight();
+
+  $(window).scroll((e) => (didScroll = true));
+
+  setInterval(() => {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+      // Scroll Down
+      $('nav').removeClass('nav-down').addClass('nav-up');
+      $('.nav-up').css('top', -$('nav').outerHeight() + 'px');
+    } else {
+      // Scroll Up
+      if (st + $(window).height() < $(document).height()) {
+        $('nav').removeClass('nav-up').addClass('nav-down');
+        $('.nav-up, .nav-down').css('top', '0px');
+      }
+    }
+
+    lastScrollTop = st;
+  }
+
+  $('.site-content').css('margin-top', $('header').outerHeight() + 'px');
+
   // scroll body to 0px on click
   $('#back-to-top').click(function () {
     $('body,html').animate(
